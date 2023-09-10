46b8f94d14a233de8cfa282df59eaaff78f2ad86
KayTee1 <kha.truong@tuni.fi>
2023-09-10 16:52:05 +0300

    fixed bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import TestApp from "./testapp";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
